c01f127a1c7d991106b778d518c05be3c4ffcb7f
Ching Wong <ching.wong15@imperial.ac.uk>
2016-02-15 09:31:59 +0000

    15_2_16 9:30 Stage 1

--- src/spreadsheet/Main.java
+++ src/spreadsheet/Main.java
@@ -1,12 +1,29 @@
 package spreadsheet;
 
 
+import spreadsheet.gui.SpreadsheetGUI;
+
 public class Main {
 
-    private static final int DEFAULT_NUM_ROWS = 5000;
-    private static final int DEFAULT_NUM_COLUMNS = 5000;
+  private static final int DEFAULT_NUM_ROWS = 5000;
+  private static final int DEFAULT_NUM_COLUMNS = 5000;
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    assert(args.length == 1 && args.length > 2): "Please enter 0 or 2 arguments.";
+
+    if(args.length == 0){
+      Spreadsheet spreadsheet = new Spreadsheet();
+      SpreadsheetGUI spreadsheetGUI
+        = new SpreadsheetGUI(spreadsheet, DEFAULT_NUM_ROWS, DEFAULT_NUM_COLUMNS);
     }
 
+    else if(args.length == 2){
+      int NUM_ROWS = Integer.parseInt(args[0]);
+      int NUM_COLUMNS = Integer.parseInt(args[1]);
+      Spreadsheet spreadsheet = new Spreadsheet();
+      SpreadsheetGUI spreadsheetGUI
+        = new SpreadsheetGUI(spreadsheet,NUM_ROWS, NUM_COLUMNS);
+    }
+    System.out.println("A spreadSheet is created");
+  }
 }
--- src/spreadsheet/Spreadsheet.java
+++ src/spreadsheet/Spreadsheet.java
@@ -1,6 +1,26 @@
 package spreadsheet;
 
+import spreadsheet.api.CellLocation;
 import spreadsheet.api.SpreadsheetInterface;
+import spreadsheet.api.value.Value;
 
 public class Spreadsheet implements SpreadsheetInterface {
+
+  public void setExpression(CellLocation location, String expression){
+
+  }
+
+  public String getExpression(CellLocation location){
+    return ""; //TODO :Check
+  }
+
+  public Value getValue(CellLocation location){
+    return null;//TODO: Check
+  }
+
+  public void recompute(){
+
+  }
+
+
 }
